e20b8fcd14ecc1a271ad76e895c4cbe8ad5d90d0
santosh 2 <santehero8@gmail.com>
2022-08-16 22:38:41 +0545

    some bugs fixed

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const app = express()
 const cors = require("cors")
 
 
-
 require("dotenv").config()
 require("./config/db")()
 
+const corsConfig = {
+    origin: process.env.ALLOWED_CLIENTS.split(",")
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsConfig))
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, '/views'))
 app.set("view engine", 'ejs')
@@ -18,6 +21,5 @@ app.set("view engine", 'ejs')
 // endpoints
 
 app.use("/api/files", require("./routes/files"))
-
 app.listen(8080, () => console.log(`server started`))
 
