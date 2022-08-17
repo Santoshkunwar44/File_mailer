0a80aca45ae192d42d103157edf3ef7a626685e7
santosh 2 <santehero8@gmail.com>
2022-08-17 16:03:04 +0545

    delte files after 24 hours script added

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDb = async () => {
+    console.log(process.env.MONGO_CONNECTION_URL)
     try {
 
         const connect = await mongoose.connect(process.env.MONGO_CONNECTION_URL, {
